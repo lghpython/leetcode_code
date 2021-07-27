f4faf44b99d0a75bda13019a6e009787fb0d20df
lghgo <lghgo@sina.com>
2021-07-27 21:16:27 +0800

    76,77

--- [76]最小覆盖子串.py
+++ [76]最小覆盖子串.py
@@ -1,42 +1,55 @@
-def check(ssum, tsum):
-    for i in tsum:
-        if ssum[i]< tsum[i]:
-            return False
-        # print(i)
-    return True
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def check(ss,ts):
+            for i in ss:
+                if ss[i] < ts[i]:
+                    return False
+            return True
 
+        ans = ''
+        tsum = {c:0 for c in t}
+        ssum = dict(tsum)
+        need = [(i,c) for i,c in enumerate(s) if c in t]
+        less = len(s)
+        sta = 0
+        for c in t:
+            tsum[c] += 1
+        for i,c in need:
+            ssum[c] += 1
+            while check(ssum,tsum):
+                if i-need[sta][0]<less:
+                    less = min(less,i-need[sta][0])
+                    ans = s[need[sta][0]:i+1]
+                ssum[need[sta][1]]-=1
+                sta += 1
+        return ans
 
-def minWindow( s: str, t: str) -> str:
-    ins =[]
-    need = []
-    for i, c in enumerate(s):
-        if c in t:
-            ins.append(i)
-            need.append(c)
-    tsum = {}
-    for k in t: tsum[k] = 0
-    ssum = dict(tsum)
-    for k in t: tsum[k]+= 1
-    left = 0
-    min = len(s)
-    ans = ''
-    for i,n in enumerate(need):
-
-        ssum[n]+=1
-        while check(ssum,tsum):
-            if ins[i] - ins[left] < min:
-                min = ins[i]- ins[left]
-                ans = s[ins[left]:ins[i]+1]
-            ssum[need[left]] -= 1
-            left += 1
-    # print(ans+'------')
-    return ans
-
-
-
-
-if __name__ == '__main__':
-    S = "ADOBECODEBANC"
-    T = "ABC"
-    result = minWindow(S,T)
-    print(result)
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        ins =[]
+        need = []
+        for i, c in enumerate(s):
+            if c in t:
+                ins.append(i)
+                need.append(c)
+        tsum = {c:0 for c in t}
+        ssum = dict(tsum)
+        for k in t: tsum[k]+= 1
+        left = 0
+        less = len(s)
+        ans = ''
+        for i,n in enumerate(need):
+            ssum[n]+=1
+            while self.check(ssum,tsum):
+                if ins[i] - ins[left] < less:
+                    less = ins[i]- ins[left]
+                    ans = s[ins[left]:ins[i]+1]
+                ssum[need[left]] -= 1
+                left += 1
+        return ans
+    def check(self, ssum, tsum):
+        for i in tsum:
+            if ssum[i]< tsum[i]:
+                return False
+        return True
---
+++ [77] 组合.py
@@ -0,0 +1,52 @@
+class Solution:
+    def combine(self, n, k):
+        res = []
+        # 思路1 [1,...,n] 的列表剔除值每次剔除 一个值 ，当列表长度为k 添加到结果到列表
+        # 思路2 [1,...,k] 从k开始一个一个挪
+        def dfs(tmp, i):
+            # 切片剔除当前值
+            nonlocal res
+            if len(tmp) == k:
+                if tmp not in res:
+                    res.append(tmp)
+                return
+            # 依次删除
+            for i in range(n):
+                # 索引大于tmp长度删除
+                if i>=len(tmp) or len(tmp[:i]) > k: break
+                dfs(tmp[:i]+ tmp[i+1:], i)
+                
+            return
+        res = []
+        all = [x for x in range(1,n+1)]
+        dfs(all,0)
+        return res
+
+class Solution2:
+    def combine(self, n, k):
+        res = []
+
+        def dfs(start, need, tmp):
+            print(start,n-start+1, need)
+            if n-start+1 < need: return # 剩余值不足
+            if need == 0: 
+                res.append(list(tmp))
+                return
+            for i in range(start,n+1):
+                # print(tmp)
+                tmp.append(i)
+                dfs(i+1,need-1,tmp)
+                tmp.pop()
+        dfs(1,k,[])
+        return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print('结果', solution.combine(4,2))
+
+    solution2 = Solution2()
+    print('结果', solution.combine(4,2))
+
+
+  
